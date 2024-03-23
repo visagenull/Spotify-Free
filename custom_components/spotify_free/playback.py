10d82047f8da487b03af4d84a48fba147abd6fee
lw-8991 <46113578+lw-8991@users.noreply.github.com>
2024-03-23 23:33:40 +0000

    Update playback.py

--- custom_components/spotify_free/playback.py
+++ custom_components/spotify_free/playback.py
@@ -14,33 +14,34 @@ class Spotify:
             "Content-Type": "application/json"
         }
 
-    @staticmethod
-    def refresh_token_decorator(api_function):
-        """Refresh access token on error."""
-        async def wrapper(self, *args, **kwargs):
-            try:
-                response = await api_function(self, *args, **kwargs)
-                if response is not None:
-                    if response["status_code"] == 401:
-                        access_token = await self.get_access_token()
-                        self._headers["Authorization"] = f"Bearer {access_token}"
-                        refreshed_response = await api_function(self, *args, **kwargs)
-                        return {"status_code": refreshed_response["status_code"], "data": refreshed_response["data"]}
-                    if response["status_code"] == 404:
-                        _LOGGER.error(response["data"])
-                    return response
-            except Exception as e:
-                _LOGGER.error(f"An error occurred during API request: {e}")
+    async def make_api_call(self, method, url, **kwargs):
+        try:
+            if not self._access_token:
+                await self.get_access_token()
 
-        return wrapper
+            async with aiohttp.ClientSession() as session:
+                async with session.request(method, url, headers=self._headers, **kwargs) as response:
+                    if response.status == 401:
+                        await self.get_access_token()
+                        response = await session.request(method, url, headers=self._headers, **kwargs)
+
+                    if response.content_type == "application/json":
+                        data = await response.json()
+                    else:
+                        data = await response.text()
+                        
+                    return {"status_code": response.status, "data": data}
+        except aiohttp.ClientError as e:
+            _LOGGER.error(f"An error occurred during API request: {e}")
+            return None
+        except Exception as e:
+            _LOGGER.error(f"An unexpected error occurred: {e}")
+            return None
 
     async def get_access_token(self):
         """Request access token from Spotify."""
-
         url = 'https://open.spotify.com/get_access_token?'
-
         cookies = {'sp_dc': self._sp_dc}
-
         headers = {
             'user-agent': (
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -60,78 +61,53 @@ class Spotify:
                         return access_token
                     else:
                         print(f"Error: {response}")
-
         except Exception as e:
             _LOGGER.error(f"Error: {str(e)}")
 
-    @refresh_token_decorator
-    async def get_user_profile(self): # Get user's Spotify profile name from SP_DC.
-        async with aiohttp.ClientSession() as session:
-            async with session.get("https://api.spotify.com/v1/me", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def get_user_profile(self):
+        """Get user's Spotify profile name from SP_DC."""
+        return await self.make_api_call("GET", "https://api.spotify.com/v1/me")
 
-    @refresh_token_decorator
-    async def pause(self): # Pause playback.
-        async with aiohttp.ClientSession() as session:
-            async with session.put("https://api.spotify.com/v1/me/player/pause", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def pause(self):
+        """Pause playback."""
+        return await self.make_api_call("PUT", "https://api.spotify.com/v1/me/player/pause")
 
-    @refresh_token_decorator
-    async def resume(self): # Resume playback.
-        async with aiohttp.ClientSession() as session:
-            async with session.put("https://api.spotify.com/v1/me/player/play", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def resume(self):
+        """Resume playback."""
+        return await self.make_api_call("PUT", "https://api.spotify.com/v1/me/player/play")
 
-    @refresh_token_decorator
-    async def get_playback_status(self): # Get info about current playing song and device.
-        async with aiohttp.ClientSession() as session:
-            async with session.get("https://api.spotify.com/v1/me/player", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def get_playback_status(self):
+        """Get info about current playing song and device."""
+        return await self.make_api_call("GET", "https://api.spotify.com/v1/me/player")
 
-    @refresh_token_decorator
-    async def next(self): # Skip to next track.
-        async with aiohttp.ClientSession() as session:
-            async with session.post("https://api.spotify.com/v1/me/player/next", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def next(self):
+        """Skip to next track."""
+        return await self.make_api_call("POST", "https://api.spotify.com/v1/me/player/next")
 
-    @refresh_token_decorator
-    async def previous(self): # Skip to revious track.
-        async with aiohttp.ClientSession() as session:
-            async with session.post("https://api.spotify.com/v1/me/player/previous", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def previous(self):
+        """Skip to previous track."""
+        return await self.make_api_call("POST", "https://api.spotify.com/v1/me/player/previous")
 
-    @refresh_token_decorator
-    async def seek(self, seek_ms): # Seek to position in milliseconds.
-        async with aiohttp.ClientSession() as session:
-            async with session.put(f"https://api.spotify.com/v1/me/player/seek?position_ms={seek_ms}", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def seek(self, seek_ms):
+        """Seek to position in milliseconds."""
+        return await self.make_api_call("PUT", f"https://api.spotify.com/v1/me/player/seek?position_ms={seek_ms}")
 
-    @refresh_token_decorator
-    async def volume_percent(self, volume_percent): # Set volume percentage.
-        async with aiohttp.ClientSession() as session:
-            async with session.put(f"https://api.spotify.com/v1/me/player/volume?volume_percent={volume_percent}", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def volume_percent(self, volume_percent):
+        """Set volume percentage."""
+        return await self.make_api_call("PUT", f"https://api.spotify.com/v1/me/player/volume?volume_percent={volume_percent}")
 
-    @refresh_token_decorator
-    async def set_shuffle(self, shuffle): # Set shuffle mode
-        async with aiohttp.ClientSession() as session:
-            async with session.put(f"https://api.spotify.com/v1/me/player/shuffle?state={shuffle}", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def set_shuffle(self, shuffle):
+        """Set shuffle mode."""
+        return await self.make_api_call("PUT", f"https://api.spotify.com/v1/me/player/shuffle?state={shuffle}")
 
-    @refresh_token_decorator
-    async def set_smart_shuffle(self, smart_shuffle): # Set smart shuffle mode, currently unused.
-        async with aiohttp.ClientSession() as session:
-            async with session.put(f"https://api.spotify.com/v1/me/player/smart_shuffle?state={smart_shuffle}", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def set_smart_shuffle(self, smart_shuffle):
+        """Set smart shuffle mode, currently unused."""
+        return await self.make_api_call("PUT", f"https://api.spotify.com/v1/me/player/smart_shuffle?state={smart_shuffle}")
 
-    @refresh_token_decorator
-    async def set_repeat(self, repeat): # Set repeat mode.
-        async with aiohttp.ClientSession() as session:
-            async with session.put(f"https://api.spotify.com/v1/me/player/repeat?state={repeat}", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def set_repeat(self, repeat):
+        """Set repeat mode."""
+        return await self.make_api_call("PUT", f"https://api.spotify.com/v1/me/player/repeat?state={repeat}")
 
-    @refresh_token_decorator
-    async def select_device(self, device, control_device): # Change playback device.
-        async with aiohttp.ClientSession() as session:
-            async with session.post(f"https://gew1-spclient.spotify.com/connect-state/v1/connect/transfer/from/{control_device}/to/{device}", headers=self._headers) as response:
-                return {"status_code": response.status, "data": await response.json()}
+    async def select_device(self, device, control_device):
+        """Change playback device."""
+        return await self.make_api_call("POST", f"https://gew1-spclient.spotify.com/connect-state/v1/connect/transfer/from/{control_device}/to/{device}")
