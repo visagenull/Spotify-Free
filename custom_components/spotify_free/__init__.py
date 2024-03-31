5e4acc11564366e7fcdf73ef07c4bb347474f37c
lw-8991 <46113578+lw-8991@users.noreply.github.com>
2024-03-31 10:05:00 +0100

    Update __init__.py

--- custom_components/spotify_free/__init__.py
+++ custom_components/spotify_free/__init__.py
@@ -8,11 +8,11 @@ from .const import DOMAIN
 PLATFORMS = ["media_player"]
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the my_media_player component."""
+    """Set up the spotify_free component."""
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up my_media_player from a config entry."""
+    """Set up media player from a config entry."""
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "media_player")
     )
