24934bded07df0666d32b1bd9a5d28fd5a5ddcbc
visagenull <46113578+visagenull@users.noreply.github.com>
2025-05-21 21:09:59 +0100

    stopped some silly errors

--- custom_components/spotify_free/__init__.py
+++ custom_components/spotify_free/__init__.py
@@ -13,9 +13,8 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up media player from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "media_player")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
